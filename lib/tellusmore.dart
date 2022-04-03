1b333fac64b042b402a5dc35283bdc042df23b6f
Shaheer Ahmed <shaheer.ahmed12@gmail.com>
2022-04-03 22:40:59 +0500

    Fixed Tell Us More Screen Alignment

--- lib/tellusmore.dart
+++ lib/tellusmore.dart
@@ -18,63 +18,63 @@ class _tellusmorePageState extends State<tellusmorePage> {
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.of(context).size.height;
     final double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       body: Center(
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              SizedBox(height: screenHeight * 0.1),
+              //SizedBox(height: screenHeight * 0.1),
               // Logo
-              logo(),
-              SizedBox(height: screenHeight * 0.05),
+              logo(
+                fontSize: screenHeight * 0.06,
+              ),
+              SizedBox(height: screenHeight * 0.04),
               // Login Banner
               Text(
                 'Tell Us More',
                 style: GoogleFonts.poppins(
                   color: const Color.fromARGB(255, 59, 59, 61),
-                  fontSize: screenHeight * 0.05,
+                  fontSize: screenHeight * 0.07,
                   fontWeight: FontWeight.w900,
                 ),
               ),
-              SizedBox(height: screenHeight * 0.04),
-              Container(
-                width: screenHeight * 0.8,
-                child: Padding(
-                  padding:
-                      const EdgeInsets.only(left: 50, right: 50, bottom: 10),
-                  child: Column(children: <Widget>[
-                    // Name TextField
-                    gocartTextField(
-                        hint: "Name", control: TextEditingController()),
-                    // Address TextField
-                    gocartTextField(
-                      hint: "Address",
-                      control: TextEditingController(),
-                      textType: TextInputType.streetAddress,
-                    ),
-                    // City TextField
-                    gocartTextField(
-                      hint: "City",
-                      control: TextEditingController(),
-                    ),
-                    // Phone Number TextField
-                    gocartTextField(
-                      hint: "Phone Number",
-                      control: TextEditingController(),
-                      textType: TextInputType.number,
-                    ),
-                  ]),
-                ),
+              SizedBox(height: screenHeight * 0.06),
+              Padding(
+                padding: const EdgeInsets.only(left: 40, right: 40, bottom: 10),
+                child: Column(children: <Widget>[
+                  // Name TextField
+                  gocartTextField(
+                      hint: "Name", control: TextEditingController()),
+                  // Address TextField
+                  gocartTextField(
+                    hint: "Address",
+                    control: TextEditingController(),
+                    textType: TextInputType.streetAddress,
+                  ),
+                  // City TextField
+                  gocartTextField(
+                    hint: "City",
+                    control: TextEditingController(),
+                  ),
+                  // Phone Number TextField
+                  gocartTextField(
+                    hint: "Phone Number",
+                    control: TextEditingController(),
+                    textType: TextInputType.number,
+                  ),
+                ]),
               ),
+              SizedBox(height: screenHeight * 0.02),
               // Login Button
               coolButton(
-                  text: "Register",
+                  text: "Finish",
                   functionToComply: () {
                     Navigator.push(context,
                         MaterialPageRoute(builder: (context) => homePage()));
                   }),
-              SizedBox(height: screenHeight * 0.02),
+              SizedBox(height: 10),
               // Fill Details Later?
               Row(
                   mainAxisAlignment: MainAxisAlignment.center,
