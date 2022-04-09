e7ada9b7803e83da20e3bcd1f05aa17460d96730
Shaheer Ahmed <shaheer.ahmed12@gmail.com>
2022-04-09 21:32:23 +0500

    Implemented Filter Button
    - Price Range incomplete

--- lib/Main Screen Pages/search_page.dart
+++ lib/Main Screen Pages/search_page.dart
@@ -55,7 +55,92 @@ class _SearchPageState extends State<SearchPage> {
           width: 10,
         ),
         InkWell(
-          onTap: () {},
+          onTap: () async {
+            bool isFilter = false;
+            isFilter = await showDialog(
+                barrierDismissible: false,
+                context: context,
+                builder: (context) {
+                  return StreamBuilder<Object>(
+                      stream: null,
+                      builder: (context, snapshot) {
+                        return SimpleDialog(
+                          title: const Text("Filter Search"),
+                          children: [
+                            const FilterOptionWidget(
+                              items: <String>[
+                                'T-Shirt',
+                                'Jeans',
+                                'Mask',
+                                'Specs',
+                                'Kurtis',
+                                'Shoes',
+                              ],
+                              title: "Category",
+                            ),
+                            const FilterOptionWidget(
+                              items: <String>[
+                                'Black',
+                                'Red',
+                                'Yellow',
+                                'Blue',
+                              ],
+                              title: "Colors",
+                            ),
+                            const FilterOptionWidget(
+                              items: <String>[
+                                'Small',
+                                'Medium',
+                                'Large',
+                                'Extra Large'
+                              ],
+                              title: "Size",
+                            ),
+                            const FilterOptionWidget(
+                              items: <String>[
+                                'Yes',
+                                'No',
+                              ],
+                              title: "AR Compatible",
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 32.0, right: 32),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop(false);
+                                    },
+                                    child: const Text(
+                                      "Cancel",
+                                      style: TextStyle(
+                                        color: Colors.red,
+                                      ),
+                                    ),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop(true);
+                                    },
+                                    child: const Text(
+                                      "Filter",
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        );
+                      });
+                });
+          },
           child: Text(
             'Filter Search',
             style: GoogleFonts.poppins(
@@ -92,6 +177,73 @@ class _SearchPageState extends State<SearchPage> {
           setState(() {});
         },
       ),
+    );
+  }
+}
+
+class FilterOptionWidget extends StatelessWidget {
+  const FilterOptionWidget({
+    Key? key,
+    required this.items,
+    required this.title,
+  }) : super(key: key);
+
+  final List<String> items;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 32.0, right: 32),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text("$title: "),
+          DropDownWidget(
+            items: items,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DropDownWidget extends StatefulWidget {
+  const DropDownWidget({Key? key, required this.items}) : super(key: key);
+
+  final List<String> items;
+
+  @override
+  State<DropDownWidget> createState() => _DropDownWidgetState();
+}
+
+class _DropDownWidgetState extends State<DropDownWidget> {
+  String dropdownValue = "";
+
+  @override
+  void initState() {
+    dropdownValue = widget.items.first;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.arrow_drop_down_rounded),
+      elevation: 16,
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownValue = newValue!;
+        });
+      },
+      items: widget.items.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: SizedBox(width: 75, child: Text(value)),
+        );
+      }).toList(),
     );
   }
 }
