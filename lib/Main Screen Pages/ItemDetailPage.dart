a970a06af3927d434312ec15e558d16bcc4a6278
Ashad Nadeem Mahmudi <ashadnadeem@gmail.com>
2022-05-14 05:30:29 +0500

    Add Image Carousel with Manual Scroll

--- lib/Main Screen Pages/ItemDetailPage.dart
+++ lib/Main Screen Pages/ItemDetailPage.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:gocart/Models/cart_provider.dart';
 import 'package:gocart/Models/item_model.dart';
@@ -18,35 +19,84 @@ class ItemDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.of(context).size.height;
+    CarouselController swipeController = CarouselController();
     return Scaffold(
       // backgroundColor: Colors.transparent,
       appBar: const MyAppBar(implyLeading: true),
-      body: Center(
-        child: Stack(children: <Widget>[
-          BackDrop(product: product),
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(height: screenHeight * 0.35),
-                ProductDrawer(product: product),
-              ],
+      body: GestureDetector(
+        onPanUpdate: (details) {
+          // Swiping in right direction.
+          if (details.delta.dx > 0) {
+            print("Swiping in right direction");
+            swipeController.previousPage(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.linear);
+          }
+          // Swiping in left direction.
+          if (details.delta.dx < 0) {
+            print("Swiping in left direction");
+            swipeController.nextPage(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.linear);
+          }
+        },
+        child: Center(
+          child: Stack(children: <Widget>[
+            BackDrop(product: product, swipeController: swipeController),
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(height: screenHeight * 0.35),
+                  ProductDrawer(product: product),
+                ],
+              ),
             ),
-          ),
-        ]),
+          ]),
+        ),
       ),
     );
   }
 }
 
 class BackDrop extends StatelessWidget {
-  const BackDrop({required this.product, Key? key}) : super(key: key);
+  const BackDrop(
+      {required this.product, required this.swipeController, Key? key})
+      : super(key: key);
   final Item product;
+  final CarouselController swipeController;
   @override
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.of(context).size.height;
+    print(product.images);
+    Widget imageCarousel() {
+      return CarouselSlider(
+          items: product.images.map((url) {
+            return Image.network(url, fit: BoxFit.cover);
+          }).toList(),
+          carouselController: swipeController,
+          options: CarouselOptions(
+            height: double.infinity,
+            aspectRatio: 16 / 9,
+            viewportFraction: 0.8,
+            initialPage: 0,
+            enableInfiniteScroll: false,
+            reverse: false,
+            autoPlay: true,
+            autoPlayInterval: const Duration(seconds: 3),
+            autoPlayAnimationDuration: const Duration(milliseconds: 800),
+            autoPlayCurve: Curves.fastOutSlowIn,
+            enlargeCenterPage: true,
+            onPageChanged: (index, reason) {},
+            scrollDirection: Axis.horizontal,
+            onScrolled: (index) {},
+            scrollPhysics:
+                const PageScrollPhysics(parent: BouncingScrollPhysics()),
+          ));
+    }
+
     return Container(
-      child: Image.network(product.images.first),
-      width: double.infinity,
+      child: imageCarousel(),
+      width: MediaQuery.of(context).size.width,
       height: screenHeight * 0.4,
       decoration: BoxDecoration(
         image: DecorationImage(
@@ -191,90 +241,86 @@ class _ProductDrawerState extends State<ProductDrawer> {
       );
     }
 
-    return SingleChildScrollView(
-      child: Container(
-        // Define size
-        height: screenHeight,
-        // Add radius
-        decoration: const BoxDecoration(
-          color: Colors.white70,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(35),
-            topRight: Radius.circular(35),
-          ),
+    return Container(
+      // Define size
+      height: screenHeight,
+      // Add radius
+      decoration: const BoxDecoration(
+        color: Colors.white70,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(35),
+          topRight: Radius.circular(35),
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(50),
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Brand Name
-              Text(
-                  context
-                      .read<BrandProvider>()
-                      .getBrandName(widget.product.brandID),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(50),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Brand Name
+            Text(
+                context
+                    .read<BrandProvider>()
+                    .getBrandName(widget.product.brandID),
+                style: GoogleFonts.poppins(
+                    fontSize: screenHeight * 0.025,
+                    fontWeight: FontWeight.w300),
+                textAlign: TextAlign.start),
+            // Product Name And AR Camera
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  widget.product.name.split(" ").first +
+                      " " +
+                      widget.product.name.split(" ").last,
+                  maxLines: 1,
                   style: GoogleFonts.poppins(
-                      fontSize: screenHeight * 0.025,
-                      fontWeight: FontWeight.w300),
-                  textAlign: TextAlign.start),
-              // Product Name And AR Camera
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    widget.product.name.split(" ").first +
-                        " " +
-                        widget.product.name.split(" ").last,
-                    maxLines: 1,
-                    style: GoogleFonts.poppins(
-                        fontSize: screenHeight * 0.03,
-                        fontWeight: FontWeight.w600),
-                    textAlign: TextAlign.start,
-                  ),
-                  widget.product.arLink.isNotEmpty
-                      ? ArCamButton(product: widget.product)
-                      : Container(),
-                ],
-              ),
-              // Product Price
-              Text('Rs: ${widget.product.price}',
-                  style: GoogleFonts.poppins(
-                      fontSize: screenHeight * 0.02,
+                      fontSize: screenHeight * 0.03,
                       fontWeight: FontWeight.w600),
-                  textAlign: TextAlign.center),
-              SizedBox(height: screenHeight * 0.01),
-              // Category
-              Text('Category: ${widget.product.category}',
-                  style: GoogleFonts.poppins(
-                      fontSize: screenHeight * 0.02,
-                      fontWeight: FontWeight.w500),
-                  textAlign: TextAlign.start),
-              SizedBox(height: screenHeight * 0.01),
-              // Product Description
-              Text(widget.product.description,
-                  maxLines: 5,
-                  overflow: TextOverflow.ellipsis,
-                  style: GoogleFonts.poppins(
-                      fontSize: screenHeight * 0.018,
-                      fontWeight: FontWeight.w300),
-                  textAlign: TextAlign.start),
-              // Color Selector from Filter
-              // Size Selector from Filter
-              SizedBox(height: screenHeight * 0.01),
-              colorDropdownSelection(),
-              SizedBox(height: screenHeight * 0.01),
-              sizeDropdownSelection(),
-              SizedBox(height: screenHeight * 0.02),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  addToCartButon(),
-                  FavButton(item: widget.product),
-                ],
-              ),
-            ],
-          ),
+                  textAlign: TextAlign.start,
+                ),
+                widget.product.arLink.isNotEmpty
+                    ? ArCamButton(product: widget.product)
+                    : Container(),
+              ],
+            ),
+            // Product Price
+            Text('Rs: ${widget.product.price}',
+                style: GoogleFonts.poppins(
+                    fontSize: screenHeight * 0.02, fontWeight: FontWeight.w600),
+                textAlign: TextAlign.center),
+            SizedBox(height: screenHeight * 0.01),
+            // Category
+            Text('Category: ${widget.product.category}',
+                style: GoogleFonts.poppins(
+                    fontSize: screenHeight * 0.02, fontWeight: FontWeight.w500),
+                textAlign: TextAlign.start),
+            SizedBox(height: screenHeight * 0.01),
+            // Product Description
+            Text(widget.product.description,
+                maxLines: 5,
+                overflow: TextOverflow.ellipsis,
+                style: GoogleFonts.poppins(
+                    fontSize: screenHeight * 0.018,
+                    fontWeight: FontWeight.w300),
+                textAlign: TextAlign.start),
+            // Color Selector from Filter
+            // Size Selector from Filter
+            SizedBox(height: screenHeight * 0.01),
+            colorDropdownSelection(),
+            SizedBox(height: screenHeight * 0.01),
+            sizeDropdownSelection(),
+            SizedBox(height: screenHeight * 0.02),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                addToCartButon(),
+                // FavButton(item: widget.product),
+              ],
+            ),
+          ],
         ),
       ),
     );
